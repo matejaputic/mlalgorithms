dcd447f37b8aac998a9d76cb182e93b2f475264e
Tommy Tracy II <tjt7a@virginia.edu>
2016-09-27 17:44:59 -0400

    Added y functionality and improved printout info

--- src/boost_tokenizer.cpp
+++ src/boost_tokenizer.cpp
@@ -1,3 +1,10 @@
+/*
+    This version of the tokenizer parses floats.
+    The first <N-1> columns are parsed into X
+    The last column is parsed into Y
+
+*/
+
 #include <iostream>     // cout, endl
 #include <fstream>      // fstream
 #include <sstream>      // sstream
@@ -16,24 +23,40 @@ int main()
     string line;
 
     vector< float > vec;
-    vector<vector<float>> X;
+    vector<vector<float> > X;
     vector<float> y;
 
+    // Read line by line
     while (getline(in_data, line))
     {
         string column;
         stringstream line_stream(line);
+
+        // Delimit the columns on ','
         while (getline(line_stream, column, ',')) {
             vec.push_back(stof(column));
         }
+
+        // Use the last column for y
+        y.push_back(vec.back());
+
+        // Push the rest into X
+        vec.pop_back();
         X.push_back(vec);
+        
         vec.clear();
     }
 
+    cout << "----- X -----" << endl;;
     for(auto i: X) {
         for(auto j: i){
             cout << j << " ";
         }
         cout << endl;
+    }
+
+    cout << "----- y -----" << endl;
+    for(auto i: y){
+        cout << i << endl;
     }
 }
